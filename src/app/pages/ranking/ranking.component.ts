1cb2fabaf1725612b5a0a95d2e2e143f82e24e15
Braulio De Leon <hi@brauliodeleon.com>
2023-04-02 03:21:32 -0300

    feat: add ranking layout

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UsersService } from 'src/app/core/services/users.service';
+import { map, Observable, of } from 'rxjs';
+import { User } from 'src/app/core/models/user';
 
 @Component({
   selector: 'app-ranking',
@@ -8,6 +12,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.scss']
 })
-export class RankingComponent {
+export class RankingComponent implements OnInit {
+  private readonly _router = inject(ActivatedRoute);
+  private readonly _userService = inject(UsersService);
+  users$: Observable<User[]> = of([]);
+  teamCode = '';
 
+  ngOnInit(): void {
+    const teamCode = this._router.snapshot.queryParams['teamCode'];
+
+    this.teamCode = teamCode;
+    this.users$ = this._userService.getUsersByTeamCode(teamCode).pipe(
+      map(users => users.sort((a, b) => a.score > b.score ? -1 : 1))
+    );
+  }
 }
