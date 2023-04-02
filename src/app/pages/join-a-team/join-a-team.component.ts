51bdcca4ee08cd5c39c23bd173f84f9928e7e2b4
Braulio De Leon <hi@brauliodeleon.com>
2023-04-02 01:57:38 -0300

    feat: create some reusable components to clean up the layout

--- src/app/pages/join-a-team/join-a-team.component.ts
+++ src/app/pages/join-a-team/join-a-team.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LabelComponent } from '../../shared/components/label/label.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-join-a-team',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, LabelComponent, ButtonComponent],
   templateUrl: './join-a-team.component.html',
   styleUrls: ['./join-a-team.component.scss']
 })
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() inputType: 'button' | 'submit' = 'button';
+  @Input() value = '';
+  @Input() isDisabled = false;
+}
