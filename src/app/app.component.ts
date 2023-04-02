c07f85db96150e7c47d1e344cab52ff895d0864d
Braulio De Leon <hi@brauliodeleon.com>
2023-04-01 22:59:54 -0300

    feat: add join a team layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterOutlet],
   template: `<router-outlet></router-outlet>`,
 
 })
--- src/app/pages/join-a-team/join-a-team.component.ts
+++ src/app/pages/join-a-team/join-a-team.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-join-a-team',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './join-a-team.component.html',
   styleUrls: ['./join-a-team.component.scss']
 })
 export class JoinATeamComponent {
+  private readonly _router = inject(Router);
+  private readonly _formBuilder = inject(FormBuilder);
+
+  joinTeamForm: FormGroup = this._formBuilder.group({
+    username: ['', Validators.required],
+    teamCode: ['', Validators.required]
+  });
+
+  onSubmit(): void {
+    if (this.joinTeamForm.valid) {
+      console.log(this.joinTeamForm.value);
+      this._router.navigate(['/ranking']);
+    }
+  }
 
 }
