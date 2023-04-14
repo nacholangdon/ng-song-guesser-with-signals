7db2bf755d014b00258d20eeb39ef96f89c543c8
Braulio De Leon <hi@brauliodeleon.com>
2023-04-14 18:10:07 -0300

    feat: add more rules on guess the song page

--- src/app/pages/guess-the-song/guess-the-song.component.ts
+++ src/app/pages/guess-the-song/guess-the-song.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-import { BehaviorSubject, combineLatest, interval, map, Observable, startWith, switchMap, take, takeWhile } from 'rxjs';
+import { BehaviorSubject, combineLatest, delay, filter, interval, map, Observable, startWith, switchMap, take, takeWhile, tap } from 'rxjs';
 
 import { Song } from 'src/app/core/models/song';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { SongsService } from 'src/app/core/services/songs.service';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { UsersService } from 'src/app/core/services/users.service';
 
 const PHRASE_INTERVAL = 5000;
 const COUNTDOWN_INTERVAL = 1000;
@@ -27,6 +28,12 @@ export class GuessTheSongComponent {
   private readonly _fb = inject(FormBuilder);
   private readonly _authService = inject(AuthService);
   private readonly _songsService = inject(SongsService);
+  private readonly _userService = inject(UsersService);
+
+  public attempts = 0;
+  public playedGames = 0;
+  public totalScore = 0;
+  public isGameOver = false;
 
   public teamCode = '';
   public randomSongId = Math.floor(Math.random() * 8) + 1;
@@ -47,7 +54,7 @@ export class GuessTheSongComponent {
 
   public songs$: Observable<Song[]> = this._songsService.getSongs().pipe(
     take(1),
-    map(songs => this._getSongOptions(songs, this.randomSongId, 3))
+    map(songs => this._getSongOptions(songs, this.randomSongId, 5))
   );
 
   public currentPhrase$: Observable<string> = interval(PHRASE_INTERVAL).pipe(
@@ -77,13 +84,55 @@ export class GuessTheSongComponent {
   }
 
   public onSubmit() {
+    this.attempts++;
     const selectedSongId = this.songsForm.get('songChoice')?.value;
     if (Number(selectedSongId) === this.randomSongId) {
       console.log('isCorrect -> ', this.randomSongId);
-      this.stopCondition$.next(true);
+
+      // Update playedGames and totalScore
+      this.playedGames++;
+      this.totalScore += this.attempts === 1 ? 5 : this.attempts === 2 ? 3 : 1;
+
+      // Reset attempts and select another song
+      this.attempts = 0;
+      this.randomSongId = Math.floor(Math.random() * 8) + 1;
+      this.lyrics$ = this._getLyrics(this.randomSongId);
+      this.songs$ = this._songsService.getSongs().pipe(
+        take(1),
+        map(songs => this._getSongOptions(songs, this.randomSongId, 5))
+      );
+      this.stopCondition$.next(false);
     } else {
       console.log('incorrect => ', Number(selectedSongId), this.randomSongId);
     }
+
+    if (this.attempts >= 3) {
+      this.gameOver();
+    }
+  }
+
+  private gameOver() {
+    this.isGameOver = true;
+    this.stopCondition$.next(true);
+
+    // Send score object to UserService
+    this._authService.authState$
+      .pipe(
+        take(1),
+        filter(authState => !!authState),
+        tap(authState => {
+          const scoreObject = {
+            email: authState.email,
+            playedGames: this.playedGames,
+            totalScore: this.totalScore
+          };
+          this._userService.updateUserScore(scoreObject);
+        }),
+        delay(3000)
+      )
+      .subscribe(() => {
+        this._router.navigate(['/ranking']);
+      });
   }
 
   private _getLyrics(songId: number): Observable<string[]> {
