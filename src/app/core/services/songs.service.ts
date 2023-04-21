577c5168dd9e3d56cf78c3ab4d0acf961c462b8f
Nacho Langdon <nacholangdon@gmail.com>
2023-04-20 22:04:48 -0300

    Fix repetitive http calls

--- src/app/core/services/songs.service.ts
+++ src/app/core/services/songs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Song } from '../models/song';
 
@@ -13,15 +13,9 @@ export class SongsService {
   private _http = inject(HttpClient);
 
   getSongs(): Observable<Song[]> {
+    console.log('esto no deberia ejecutarse mas de una vez');
     const headers = new HttpHeaders().append('appKey', 'YOUR_APP_KEY');
     return this._http.get<Song[]>('http://137.184.76.157:3000/song/5', { headers });
-  }
-
-  getLyrics(songPosId: number): Observable<string[]> {
-    return this.getSongs().pipe(
-      map(songs => songs[songPosId]),
-      map(song => song?.lyrics ?? [])
-    );
   }
 
 }
