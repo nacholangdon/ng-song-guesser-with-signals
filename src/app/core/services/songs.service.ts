01676239220f9c38c701fe70309553e375934f00
Braulio De Leon <hi@brauliodeleon.com>
2023-04-21 12:00:48 -0300

    feat: bypass the join-a-team page

--- src/app/core/services/songs.service.ts
+++ src/app/core/services/songs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, take, tap } from 'rxjs';
 
 import { Song } from '../models/song';
 import { Constants } from '../models/constant';
@@ -14,9 +14,8 @@ export class SongsService {
   private _http = inject(HttpClient);
 
   getSongs(): Observable<Song[]> {
-    console.log('esto no deberia ejecutarse mas de una vez');
     const headers = new HttpHeaders().append('appKey', 'YOUR_APP_KEY');
-    return this._http.get<Song[]>(`${Constants.API_URL}/song/${Constants.SONGS_OPTIONS}`, { headers });
+    return this._http.get<Song[]>(`${Constants.API_URL}/song/${Constants.SONGS_OPTIONS}`, { headers })
   }
 
 }
--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -2,37 +2,33 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 
-import { map, Observable, of } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 
 import { User } from 'src/app/core/models/user';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { UsersService } from 'src/app/core/services/users.service';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { FeedbackWordComponent } from 'src/app/shared/components/feedback-word/feedback-word.component';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, FeedbackWordComponent],
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.scss']
 })
 export class RankingComponent implements OnInit {
 
   private readonly _router = inject(Router);
-  private readonly _route = inject(ActivatedRoute);
   private readonly _userService = inject(UsersService);
   private readonly _authService = inject(AuthService);
 
   authState$ = this._authService.authState$;
   users$: Observable<User[]> = of([]);
-  teamCode = '';
 
   public ngOnInit(): void {
-    const teamCode = this._route.snapshot.queryParams['teamCode'] || 'teamA1';
-
-    this.teamCode = teamCode;
-    this.users$ = this._userService.getUsersByTeamCode(teamCode).pipe(
-      map(users => users.sort((a, b) => a.score > b.score ? -1 : 1))
+    this.users$ = this._userService.getUsers().pipe(
+      map(users => users.sort((a, b) => a.score > b.score ? -1 : 1)),
     );
 
     this.authState$.subscribe(res => {
